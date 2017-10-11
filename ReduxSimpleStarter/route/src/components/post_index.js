244b69eae5d131d7ea6a7a29976f6c13063d1dc2
José Vilamitjana <vilamitjana.jose@AxaSolutions.onmicrosoft.com>
2017-10-11 17:02:31 +0200

    cambios

--- ReduxSimpleStarter/route/src/components/post_index.js
+++ ReduxSimpleStarter/route/src/components/post_index.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostIndex extends Component {
-    render() {
-        return (
-            <div>
-                Post Index
-            </div>
-        )
-    }
-}
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-export default PostIndex;
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Post Index</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
